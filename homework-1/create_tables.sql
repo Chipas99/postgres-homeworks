50cf08313554b22cd16775649d5581eedc7bdac8
egorn <egornich99@mail.ru>
2023-11-30 17:09:39 +0300

    Заполнил файлы для HW-1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,24 @@
 -- SQL-команды для создания таблиц
+
+-- Создание таблицы employees
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    department VARCHAR(100),
+    salary DECIMAL(10, 2)
+);
+
+-- Создание таблицы customers
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    city VARCHAR(100)
+);
+
+-- Создание таблицы orders
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    customer_id INT,
+    total_amount DECIMAL(10, 2),
+    FOREIGN KEY (customer_id) REFERENCES customers(id)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,41 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+# Подключение к базе данных
+conn = psycopg2.connect(
+    dbname="north",
+    user="postgres",
+    password="Vhodanet19.",
+    host="localhost"
+)
+cur = conn.cursor()
+
+
+# Чтение данных из файла customers_data.csv и добавление их в базу данных
+with open('north_data/customers_data.csv', newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    next(csvreader)  # Пропуск заголовков
+    for row in csvreader:
+        cur.execute("INSERT INTO customers (customer_id, company_name, contact_name) VALUES (%s, %s, %s)", (row[0], row[1], row[2]))
+
+
+# Чтение данных из файла employees_data.csv и добавление их в базу данных
+with open('north_data/employees_data.csv', newline='', encoding='utf-8') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    next(csvreader)  # Пропуск заголовков
+    for row in csvreader:
+        cur.execute("INSERT INTO employees (employee_id, first_name, last_name, title, birth_date, notes) VALUES (%s, %s, %s, %s, %s, %s)", (row[0], row[1], row[2], row[3], row[4], row[5]))
+
+# Чтение данных из файла orders_data.csv и добавление их в базу данных
+with open('north_data/orders_data.csv', newline='', encoding='utf-8') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    next(csvreader)  # Пропуск заголовков
+    for row in csvreader:
+        cur.execute("INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city) VALUES (%s, %s, %s, %s, %s)", (row[0], row[1], row[2], row[3], row[4]))
+
+# Сохранение изменений
+conn.commit()
+
+# Закрытие соединения
+cur.close()
+conn.close()
